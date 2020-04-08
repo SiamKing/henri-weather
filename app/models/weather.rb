ea034b18b90edca4f6d1a1b7610338b6bd4448ad
SiamKing <thesiameseelectric@gmail.com>
2020-04-08 16:08:20 -0700

    Added test for average_temp and code for method

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -1,5 +1,11 @@
 class Weather
-    include ActiveModel::Model
-    attr_accessor :current_temp, :low_temp, :high_temp
+  include ActiveModel::Model
+  attr_accessor :current_temp, :low_temp, :high_temp
 
+  def average_temp
+    ((@low_temp + @high_temp) / 2)
+  end
+
+  def convert_temps
+  end
 end
--- spec/models/weather_spec.rb
+++ spec/models/weather_spec.rb
@@ -1,20 +1,31 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Weather, type: :model do
-    temps = { current_temp: 80, low_temp: 70, high_temp: 85 }
-    let (:weather) { Weather.new(temps) }
+  temps = { current_temp: 80, low_temp: 70, high_temp: 85 }
+  let (:weather) { Weather.new(temps) }
 
-    describe 'Weather object' do
-        it "has a current temperature" do
-            expect(weather.current_temp).to eq 80
-        end
-
-        it 'has a low temp' do
-            expect(weather.low_temp).to eq 70
-        end
-
-        it 'has a high temp' do
-            expect(weather.high_temp).to eq 85
-        end
+  describe "Weather object" do
+    it "has a current temperature" do
+      expect(weather.current_temp).to eq 80
     end
+
+    it "has a low temp" do
+      expect(weather.low_temp).to eq 70
+    end
+
+    it "has a high temp" do
+      expect(weather.high_temp).to eq 85
+    end
+  end
+
+  describe "public instance methods" do
+    context "responds to its methods" do
+      it { expect(weather).to respond_to(:convert_temps) }
+      it { expect(weather).to respond_to(:average_temp) }
+    end
+
+    describe ".average_temp" do
+      it { expect(weather.average_temp).to eq 77 }
+    end
+  end
 end
