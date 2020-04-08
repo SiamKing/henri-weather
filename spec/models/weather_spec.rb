b25c10a735d8d4556cb9f59779825e1fea8bc460
SiamKing <thesiameseelectric@gmail.com>
2020-04-08 15:32:32 -0700

    Added tests for Weather object

--- spec/models/weather_spec.rb
+++ spec/models/weather_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Weather, type: :model do
+    temps = { current_temp: 80, low_temp: 70, high_temp: 85 }
+    let (:weather) { Temp.new(temps) }
 
+    describe 'Weather object' do
+        it "has a current temperature" do
+            expect(weather.current_temp).to eq 80
+        end
+
+        it 'has a low temp' do
+            expect(weather.lo_temp).to eq 70
+        end
+
+        it 'has a high temp' do
+            expect(weather.hi_temp).to eq 85
+        end
+    end
 end
