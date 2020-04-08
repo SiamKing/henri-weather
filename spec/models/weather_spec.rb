338d62deb4590794ad17b5937ea8974b5d862dac
SiamKing <thesiameseelectric@gmail.com>
2020-04-08 16:34:18 -0700

    Updated abbreviated names and added test for temperature conversions method

--- spec/models/weather_spec.rb
+++ spec/models/weather_spec.rb
@@ -1,31 +1,38 @@
 require "rails_helper"
 
 RSpec.describe Weather, type: :model do
-  temps = { current_temp: 80, low_temp: 70, high_temp: 85 }
-  let (:weather) { Weather.new(temps) }
+  temperatures_fahrenheit = { current_temperature: 80, low_temperature: 70, high_temperature: 85 }
+  temperatures_celsius = { current_temperature: 26, low_temperature: 21, high_temperature: 29 }
+  let (:weather_f) { Weather.new(temperatures_fahrenheit) }
+  let (:weather_c) { Weather.new(temperatures_celsius) }
 
   describe "Weather object" do
     it "has a current temperature" do
-      expect(weather.current_temp).to eq 80
+      expect(weather_f.current_temperature).to eq 80
     end
 
-    it "has a low temp" do
-      expect(weather.low_temp).to eq 70
-    end
-
-    it "has a high temp" do
-      expect(weather.high_temp).to eq 85
+    it "has a low temperature" do
+      expect(weather_f.high_temperature).to eq 85
     end
   end
 
   describe "public instance methods" do
     context "responds to its methods" do
-      it { expect(weather).to respond_to(:convert_temps) }
-      it { expect(weather).to respond_to(:average_temp) }
+      it { expect(weather_f).to respond_to(:convert_temperatures) }
+      it { expect(weather_f).to respond_to(:average_temperature) }
     end
 
-    describe ".average_temp" do
-      it { expect(weather.average_temp).to eq 77 }
+    describe ".average_temperature" do
+      it { expect(weather_f.average_temperature).to eq 77 }
+    end
+
+    describe ".convert_temperatures" do
+    end
+
+    describe ".convert_temperature_to_fahrenheit" do
+      it "converts celsius temperatures to fahrenheit" do
+        expect(weather_c.convert_temperature_to_fahrenheit(weather_c.current_temperature)).to eq 80
+      end
     end
   end
 end
