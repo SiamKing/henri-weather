103e2249a8fa0ce1032c6e8a7f7b222592363fa8
SiamKing <thesiameseelectric@gmail.com>
2020-04-08 18:09:50 -0700

    Added methods for converting celsius to fahrentheit and vice versa with floats for better accuracy

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -3,12 +3,22 @@ class Weather
   attr_accessor :current_temperature, :low_temperature, :high_temperature
 
   def average_temperature
-    ((@low_temperature + @high_temperature) / 2)
+    ((@low_temperature.to_f + @high_temperature) / 2).round(2)
   end
 
-  def convert_temperatures
+  def convert_temperatures(units)
+    if units == "F"
+      as_json.each { |k, v| send("#{k}=", convert_temperature_to_fahrenheit(v)) }
+    else
+      as_json.each { |k, v| send("#{k}=", convert_temperature_to_celsius(v)) }
+    end
   end
 
-  def convert_temperature_to_fahrenheit(temperature)
+  def convert_temperature_to_fahrenheit(temp)
+    ((temp.to_f * 9 / 5) + 32).round(2)
+  end
+
+  def convert_temperature_to_celsius(temp)
+    ((temp.to_f - 32) * 5 / 9).round(2)
   end
 end
